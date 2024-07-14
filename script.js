17e5f7d70108d8e80b7af9346bd42ac5e1d65a4e
Josh Bain <joshuambain@gmail.com>
2024-07-14 16:28:05 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+//from lesson
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
+//JB Added
+const h3 = document.createElement("h3");
+h3.textContent = "And I am a minimalist";
+
+document.querySelector("body").appendChild(h3);
